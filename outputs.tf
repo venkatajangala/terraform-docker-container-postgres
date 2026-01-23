009201b709cca79a35d2ecd37a13ccb6fa642678
Venkata Jangala <venkat_rj@hotmail.com>
2026-01-23 17:57:13 -0500

    feat: integrate terraform postgres docker code with DBHub MCP docker code

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,31 @@
 output "connection_string" {
-  value     = "postgresql://${var.postgres_user}:${var.postgres_password}@localhost:5432/${var.postgres_db}"
-  sensitive = true
+  value       = "postgresql://${var.postgres_user}:${var.postgres_password}@localhost:5432/${var.postgres_db}"
+  sensitive   = true
+  description = "Connection string for PostgreSQL from host machine"
+}
+
+output "postgres_container_name" {
+  value       = docker_container.postgres.name
+  description = "Name of the PostgreSQL container"
+}
+
+output "dbhub_container_name" {
+  value       = docker_container.dbhub.name
+  description = "Name of the DBHub container"
+}
+
+output "dbhub_url" {
+  value       = "http://localhost:${var.dbhub_port}"
+  description = "DBHub web interface URL"
+}
+
+output "mcp_network" {
+  value       = docker_network.mcp_network.name
+  description = "Custom bridge network name for Postgres and DBHub communication"
+}
+
+output "postgres_dsn_internal" {
+  value       = "postgres://${var.postgres_user}:${var.postgres_password}@my-postgres:5432/${var.postgres_db}"
+  sensitive   = true
+  description = "PostgreSQL DSN for DBHub container (uses internal network DNS)"
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,24 @@
 variable "postgres_user" {
-  type    = string
-  default = "pguser"
+  type        = string
+  default     = "pgadmin"
+  description = "PostgreSQL username"
 }
 
 variable "postgres_password" {
-  type      = string
-  sensitive = true
-  default   = "change_me"
+  type        = string
+  sensitive   = true
+  default     = "pgAdmin1"
+  description = "PostgreSQL password"
 }
 
 variable "postgres_db" {
-  type    = string
-  default = "appdb"
+  type        = string
+  default     = "postgres"
+  description = "PostgreSQL database name"
+}
+
+variable "dbhub_port" {
+  type        = number
+  default     = 9090
+  description = "DBHub web interface port"
 }
